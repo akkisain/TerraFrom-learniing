cba52a495469f5615580560982d580b249ab9923
akkisain <56155479+akkisain@users.noreply.github.com>
2021-08-28 16:48:42 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAQRRIY4XUMDYV6HP4"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
